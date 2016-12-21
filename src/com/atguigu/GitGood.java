c99532987401e2afb30e72252d5a36d06679605f
王俊朋 <1832907879@qq.com>
2016-12-21 22:09:29 +0800

    1111111

--- src/com/atguigu/GitGood.java
+++ src/com/atguigu/GitGood.java
@@ -3,17 +3,17 @@
  */
 package com.atguigu;
 
-/** 
+/**
  * @包名 com.atguigu
  * @类名 GitGood.java
- * @作者  wangjunpeng
+ * @作者 wangjunpeng
  * @创建日期 2016年12月21日
- * @描述 
+ * @描述
  * @版本 V 1.0
  */
 
 public class GitGood {
-     public static void main(String[] args) {
-		System.out.println("good good study!");
+	public static void main(String[] args) {
+		System.out.println("good good study! fffffffff");
 	}
 }
