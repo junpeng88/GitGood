9389088f945a0ebedb79559a870be4179c701856
junpeng88 <1832907879@qq.com>
2016-12-21 23:48:59 +0800

    222222222222222222222

--- src/com/atguigu/GitGood.java
+++ src/com/atguigu/GitGood.java
@@ -14,6 +14,6 @@ package com.atguigu;
 
 public class GitGood {
 	public static void main(String[] args) {
-		System.out.println("good good study!444444444 fffffffff");
+		System.out.println("good good study!444444444 ffffffff------------------------------f");
 	}
 }
